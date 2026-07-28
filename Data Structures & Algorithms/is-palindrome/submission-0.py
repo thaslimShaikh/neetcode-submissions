f7f38c7354b22c274d3c3cea401ce303c829c819
Thaslim Shaik <786shaikthaslim@gmail.com>
2026-07-28 10:34:45 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        temp = ""
+        for ch in s:
+            if ch.isalnum():
+                temp += ch.lower()
+        return temp == temp[::-1]
+        
